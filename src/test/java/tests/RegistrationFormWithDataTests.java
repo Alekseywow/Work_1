dfabdeb8621d60b0266d878c6a278191730460d4
danilovaleksey <danilov20022@mail.ru>
2025-07-04 00:38:51 +0500

    added commit HW7 Part2_1

---
+++ src/test/java/tests/RegistrationFormWithDataTests.java
@@ -0,0 +1,98 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static tests.TestData.*;
+
+public class RegistrationFormWithDataTests extends TestBase{
+
+//    @BeforeAll
+//    static void setUp() {
+//        Configuration.browserSize = "1920x1080";
+//        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.pageLoadStrategy = "eager";
+//    }
+
+
+//    public static final String firstName = "Aleksey",
+//            lastName = "Danilov",
+//            userName = "user@mail.com";
+
+//    static String firstName,
+//                lastName,
+//                userName;
+
+//    @BeforeEach
+//    void  prepareTestData() {
+//                firstName = "Aleksey";
+//                lastName = "Danilov";
+//                userName = "user@mail.com";
+//    }
+
+
+    @Test
+    void formPracticeTest() {
+//
+//        String firstName = "Aleksey",
+//                lastName = "Danilov",
+//                userName = "user@mail.com";
+
+        //Открытие сайта
+        open("/automation-practice-form");
+        //Проверка заголовков
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+
+
+        //Ввод данных
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userName);
+        //Поиск по тексту и клик
+        $("#genterWrapper").$(byText("Male")).click();
+        //Ввод данных
+        $("#userNumber").setValue("7900900900");
+        //Раскрытие календаря
+        $("#dateOfBirthInput").click();
+        //Поиск по тексту и клик
+        $(".react-datepicker__month-select").$(byText("September")).click();
+        $(".react-datepicker__year-select").$(byText("1994")).click();
+        //Опишу чуть позже
+        $$(".react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text("26")).click();
+
+        //Ввод текста и нажатия энтера
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        //Поиск по тексту и клик
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        //Добавление картинки
+        $("#uploadPicture").uploadFromClasspath("cat.png");
+
+        $("#currentAddress").setValue("Some address 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+
+        //Проверка заполненных данных
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName),text(lastName));
+        $("#closeLargeModal").click();
+
+
+
+
+    }
+
+}
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class TestData {
+
+
+       public static String firstName = "Aleksey",
+                lastName = "Danilov",
+                userName = "user@mail.com";
+
+}
