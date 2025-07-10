fb4090f8f5c64474970f416ca951c9a0f44b9aa4
danilovaleksey <danilov20022@mail.ru>
2025-07-11 01:54:31 +0500

    Добавили Tag для запуска тестов

--- src/test/java/qa/guru/EmailTest.java
+++ src/test/java/qa/guru/EmailTest.java
@@ -1,9 +1,8 @@
 package qa.guru;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@Disabled("SAVC-142421")
 public class EmailTest {
 
 
@@ -14,14 +13,19 @@ public class EmailTest {
     }
 
     @Test // Запуск кейсов
+    @Tag("SMOKE")
     @DisplayName("Email должке быть отправлен забаненому юзеру")
     void emailShouldBeSentForBannedUser() {
         System.out.println("hello");
     }
 
     @Test // Запуск кейсов
+    @Tags({
+            @Tag("SMOKE"),
+            @Tag("WEB"),
+    })
+    @Tag("SMOKE")
     @DisplayName("Email должке быть отправлен в случае изменения PaymentMethod")
-    @Disabled("JDK-141312")
     void emailShouldBeSentAfterChangePaymentMethod() {
         throw new AssertionError("падаем");
     }
