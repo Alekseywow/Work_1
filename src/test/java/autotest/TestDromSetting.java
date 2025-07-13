94774fd075a26ef1e830d673bf5a09cc38e0b5de
danilovaleksey <danilov20022@mail.ru>
2025-07-14 02:31:58 +0500

    Выполнение Домашнего задания_1

---
+++ src/test/java/autotest/TestDromSetting.java
@@ -0,0 +1,15 @@
+package autotest;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestDromSetting {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://auto.drom.ru/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+}
---
+++ src/test/java/autotest/data/CarName.java
@@ -0,0 +1,7 @@
+package autotest.data;
+
+public enum CarName {
+
+    BMW,Changan,Chery,Chevrolet,Ford,Geely,Haval,Honda,Hyundai,Kia
+
+}
